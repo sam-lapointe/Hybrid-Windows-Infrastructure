d481a683d0d5953ee9038bb21c1895523ef2e5b7
sam-lapointe <samuellapointebusiness@gmail.com>
2025-03-18 14:59:40 -0400

    Added update_inventory.sh in the workflow to add a value to new hosts in the inventory file for Ansible.

---
+++ OnPrem/Terraform/update_inventory.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+vms_status="$(jq '.resource_changes[] | {actions: .change.actions, name: .change.after.name}' tfplan.json | jq -s '.')"
+
+for vm in $(echo "$vms_status" | jq -r '.[] | @base64'); do
+  _jq() { echo ${vm} | base64 --decode | jq -r ${1}; }
+  name=$(_jq '.name')
+  action=$(_jq '.actions[0]')
+  echo "A: $action"
+  if [ "$name" != "null" ] && [ "$action" == "create" ]; then
+    sed -i "/$name/ s/$/ status=new/" ./inventory
+  fi
+done
